69226695de85d630d359e192cf2654656ccfab56
Victor Pinho <victorpinho2311@github.com>
2026-06-08 22:01:33 -0300

    config: add Apps Script deployment URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Após fazer o deploy do Apps Script como Web App, cole a URL aqui.
 // Menu: Implantar > Gerenciar implantações > URL da aplicação web
 const CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/SEU_DEPLOYMENT_ID_AQUI/exec'
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxembZHQzVTOKmny3rLt-eanGupAQull0d47nKwMq0pZnEG0GTsIjrE1rDdvADALE3NOQ/exec'
 };
